3ddf61411c9ca8d1ad8fa03f9dfe6e1a21d38026
moulin-louis <louis.moulin@outlook.fr>
2023-11-13 18:15:46 +0100

    WIP encryption payload

--- src/decrypt.asm
+++ src/decrypt.asm
@@ -1,21 +1,22 @@
 section .text
-;    global decrypt ; export decrypt symbol
-    global _start
+    global decrypt ; export decrypt symbol
+;    global _start
 ;decrypt: ; void decrypt(void *key[rdi], size_t len_key[rsi], void *data[rdx], size_t len_data[rcx])
-_start:
-    push r8
-    mov r9, rdi
-    mov rdi, rdx
-    mov r8, rcx
-    mov ecx, 0x0
+decrypt:
+;    push r8
+    mov r9, rdi ; put key in r9
+    mov rdi, rdx ; put data in rdi
+    mov r8, rcx ; put len_data in r8
+    mov ecx, 0x0 ; put 0 in ecx
 start_over:
-    mov rax, rcx
-    mov edx, 0x0
-    div rsi
-    movzx eax, byte [r9 + rdx]
-    xor byte [rdi + rcx], al
-    add rcx, 0x1
-    cmp r8, rcx
-    jne start_over
-    pop r8
-    jmp r8
+    mov rax, rcx ; move rcx inside rax
+    mov edx, 0x0 ; put 0 in rdx
+    div rsi ; div rsi by rax
+    movzx eax,rcx = i % len_key
+    xor byte [rdi + rcx], al ; data[i] ^= al
+    add rcx, 0x1 ; increment rcx
+    cmp r8, rcx  ; check if we decrypt all data
+    jne start_over ; start over if not
+    ret
+;    pop r8 ; pop back value of r8
+;    jmp r8 ; jump to r8
--- src/encryption.c
+++ src/encryption.c
@@ -4,10 +4,13 @@
 
 #include "woody.h"
 
-//void xor_encrypt(void *data, size_t len, const uint8_t *key, size_t len_key) {
-void xor_encrypt(uint8_t *key, uint64_t len_key, void *data, uint64_t len_data) {
-  for (size_t i = 0; i < len_data; i++)
-    ((char *) data)[i] ^= key[i % len_key];
+void xor_encrypt(uint8_t *key, uint64_t len_key, uint8_t *data, uint64_t len_data) {
+  for (size_t i = 0; i < len_data; i++) {
+    printf("i = %zu and i = len_key = %zu ", i, i % len_key);
+    printf("key char = %#02x ", key[i % len_key]);
+    data[i] ^= key[i % len_key];
+    printf("data char = %#02x\n",data[i]);
+  }
 }
 
 int32_t encryption(t_bin *bin) {
@@ -16,13 +19,14 @@ int32_t encryption(t_bin *bin) {
   text_segment = get_segment(bin->phdrs, is_text_segment_64);
   //get key from urandom
   bin->key = calloc(1, KEY_SIZE);
-  bin->len_key = KEY_SIZE;
+//  bin->key = (uint8_t *)strdup("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
+  bin->len_key = 32;
   if (get_key(bin->key)) {
     dprintf(2, "Failed to get key\n");
     return 1;
   }
-//  printf("key: ");
-//  hexdump(bin->key, bin->len_key, 0);
+  printf("key: ");
+  hexdump(bin->key, bin->len_key, 0);
   //encrypt text segment
   void *data = bin->raw_data + text_segment->p_offset;
   xor_encrypt(bin->key, bin->len_key, data, text_segment->p_filesz);
