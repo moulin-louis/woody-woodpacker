164b22428bff1ca8aae9432521907d7e152f22f8
moulin-louis <louis.moulin@outlook.fr>
2024-01-18 12:48:17 +0100

    cleanup test

--- test.sh
+++ test.sh
@@ -13,24 +13,24 @@ function check_return_value() {
 }
 
 cd ./samples && ./build.sh > /dev/null 2>&1 && cd ..
-./cmake-build-debug/woody_woodpacker ./samples/hello > /dev/null
+./woody_woodpacker ./samples/hello > /dev/null
 ./woody > /dev/null
 check_return_value 1
-./cmake-build-debug/woody_woodpacker ./samples/hello-32 > /dev/null
+./woody_woodpacker ./samples/hello-32 > /dev/null
 ./woody > /dev/null
 check_return_value 2
-./cmake-build-debug/woody_woodpacker ./samples/hello-pie > /dev/null
+./woody_woodpacker ./samples/hello-pie > /dev/null
 ./woody > /dev/null
 check_return_value 3
-./cmake-build-debug/woody_woodpacker ./samples/hello-relocation > /dev/null 2>&1
+./woody_woodpacker ./samples/hello-relocation > /dev/null 2>&1
 ./woody > /dev/null
 check_return_value 4
-./cmake-build-debug/woody_woodpacker ./samples/no-std > /dev/null
+./woody_woodpacker ./samples/no-std > /dev/null
 ./woody > /dev/null
 check_return_value 5
-./cmake-build-debug/woody_woodpacker ./samples/nodata > /dev/null
+./woody_woodpacker ./samples/nodata > /dev/null
 ./woody > /dev/null
 check_return_value 6
-./cmake-build-debug/woody_woodpacker ./samples/tiny_c > /dev/null
+./woody_woodpacker ./samples/tiny_c > /dev/null
 ./woody > /dev/null
 check_return_value 7
